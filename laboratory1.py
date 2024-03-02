a87762ad8c3b61c49243774d1f30f6843b9ac684
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-02 23:07:26 +0300

    Add files via upload

--- laboratory1.py
+++ laboratory1.py
@@ -1,21 +1,16 @@
-import datetime
-
-
 with open('data1.txt', 'r', encoding='utf-8') as file:
     arr = [int(i) for i in file.read().split()]
 founder = int(input())
 binarr = sorted(arr)
-t1 = datetime.datetime.now(datetime.timezone.utc)
 for i in range(len(arr)):
     if founder == arr[i]:
         print(i)
-        t2 = datetime.datetime.now(datetime.timezone.utc)
         break
-print(t2 - t1)
-t3 = datetime.datetime.now(datetime.timezone.utc)
 for j in range(len(binarr)):
     if founder == binarr[j]:
         print(j)
-        t4 = datetime.datetime.now(datetime.timezone.utc)
         break
-print(t4 - t3)
+
+
+with open('tutorial.txt', 'w', encoding='utf-8') as gifa:
+    gifa.write(str(i) + ' ' + str(j) + '\n')
