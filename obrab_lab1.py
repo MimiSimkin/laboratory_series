8c53ee47be0e64a3ce71af9603dbe69620b285c9
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 15:37:20 +0300

    Update obrab_lab1.py

--- obrab_lab1.py
+++ obrab_lab1.py
@@ -1,15 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-with open('data.txt', 'r') as w:
-    t = w.readlines()
-c = ''
-g = ''
-l = []
-s = []
-a = []
-b = []
-d = 0.06
-e = 0.01
+with open('tutorial.txt', 'r') as file:
+    t = file.readlines()
+c, g = '', ''
+l, s, a, b = [], [], [], []
+d, e = 0.06, 0.01
 for i in range(499):
     a.append(int(t[i][:-1]))
 for i in range(499, 998):
@@ -18,7 +13,7 @@ for i in range(499, 998):
 x = np.array(a)
 y = np.array(b)
 
-plt.errorbar(x, y,  xerr = 0, yerr = 0, fmt='.k')
+plt.errorbar(x, y,  xerr = 0, yerr = 0, fmt='o')
 plt.grid()
 plt.ylabel('$T, c$')
 plt.xlabel('$N, эл$')
