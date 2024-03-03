90afde3fc604b064af1bdc3230eccba0fba563c3
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 13:59:34 +0300

    Add files via upload

--- number_1.py
+++ number_1.py
@@ -2,6 +2,6 @@ import random
 
 
 with open('data1.txt', 'w', encoding='utf-8') as file:
-    for i in range(100):
+    for i in range(100000):
         num = random.randint(-10**5, 10**5)
         file.write(str(num) + ' ')
