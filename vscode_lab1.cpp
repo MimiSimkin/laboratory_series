25f3049b874ce949aeda481092e36f35f4fae879
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 15:41:01 +0300

    orig_code1.cpp

--- vscode_lab1.cpp
+++ vscode_lab1.cpp
@@ -1,68 +1,37 @@
 #include <iostream>
-#include <chrono>
-#include <random>
-#include <ctime>
-#include <cstdlib>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
-void func()
-{
-    std::cout<<"HW";
-}
-
-int main()
-{
-
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int a;
-    double times[499] = {};
-    int arrays[499] = {};
-    double t;
-
-
-    unsigned seed = 1001;
-    std::default_random_engine rng(seed);
-    std::uniform_int_distribution<unsigned> dstr(0, 9);
-
-    clock_t startTime = clock();
-    for (int i = 1000; i < 500000; i+=1000){
-        int nums[i] = {}; 
-        for (unsigned counter = i; counter != 0; --counter){
-            nums[i - counter] = arr[dstr(rng)] << '\n';
-        }
-        a = nums[dstr(rng)];
-        for (int j; j < i; j++){
-            if (nums[j] == a){
-                clock_t endTime = clock();
-                double executionTime = double(endTime - t) / CLOCKS_PER_SEC;
-                times[(i - 1000)/1000] = executionTime;
-                arrays[(i - 1000)/1000] = i;
-                t = executionTime;
-                break;
-            }
+int main() {
+    std::ifstream file("data1.txt");
+    std::ofstream g("tutorial.txt", std::ios::app);
+    std::vector<int> arr;
+    int num;
+    while (file >> num) {
+        arr.push_back(num);
+    }
+    int founder;
+    std::cin >> founder;
+    std::vector<int> binarr = arr;
+    std::sort(binarr.begin(), binarr.end());
+    int i;
+    for (i = 0; i < arr.size(); i++) {
+        if (founder == arr[i]) {
+            std::cout << i << std::endl;
+            break;
         }
     }
-    std::cout<<std::endl;
-    
-    
-
-
-    std::ofstream file("data.txt");
-    if (file.is_open()) {
-        for (int i = 0; i < 499; i++) {
-            file << arrays[i] << std::endl;
+    std::cout << i << std::endl;
+    int j;
+    for (j = 0; j < binarr.size(); j++) {
+        if (founder == binarr[j]) {
+            std::cout << j << std::endl;
+            break;
         }
-        for (int i = 0; i < 499; i++) {
-            file << times[i] << std::endl;
-        }
-        file.close();
-        std::cout << "Data has been written to file data.txt" << std::endl;
-    } else {
-        std::cerr << "Unable to open file for writing" << std::endl;
-        return 1;
     }
-
-
+    std::cout << j << std::endl;
+    g << i << ' ' << j << '\n';
+    g.close();
     return 0;
 }
