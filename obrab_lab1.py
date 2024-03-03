c249fa5b6aed42b2d12c0693285ec1d1b590de78
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 20:26:07 +0300

    Update obrab_lab1.py

--- obrab_lab1.py
+++ obrab_lab1.py
@@ -2,18 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 with open('tutorial.txt', 'r') as file:
     t = file.readlines()
-c, g = '', ''
-l, s, a, b = [], [], [], []
-d, e = 0.06, 0.01
-for i in range(499):
-    a.append(int(t[i][:-1]))
-for i in range(499, 998):
-    b.append(float(t[i][:-1]))
+a, b = [], []
+for i in range(50):
+    a.append(int(t[i].split()[0]))
+for i in range(50):
+    b.append(float(t[i].split()[1]))
 
 x = np.array(a)
 y = np.array(b)
 
-plt.errorbar(x, y,  xerr = 0, yerr = 0, fmt='o')
+plt.errorbar(x, y,  xerr = 0, yerr = 0, fmt='o', color='b')
 plt.grid()
 plt.ylabel('$T, c$')
 plt.xlabel('$N, эл$')
