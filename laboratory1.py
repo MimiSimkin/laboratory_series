b641374694f32d2d9b9f3245f065eae5581e66fe
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 13:32:44 +0300

    Add files via upload

--- laboratory1.py
+++ laboratory1.py
@@ -1,16 +1,17 @@
 with open('data1.txt', 'r', encoding='utf-8') as file:
+    g = open('tutorial.txt', 'a', encoding='utf-8')
     arr = [int(i) for i in file.read().split()]
-founder = int(input())
-binarr = sorted(arr)
-for i in range(len(arr)):
-    if founder == arr[i]:
-        print(i)
-        break
-for j in range(len(binarr)):
-    if founder == binarr[j]:
-        print(j)
-        break
-
-
-with open('tutorial.txt', 'w', encoding='utf-8') as gifa:
-    gifa.write(str(i) + ' ' + str(j) + '\n')
+    founder = int(input())
+    binarr = sorted(arr)
+    for i in range(len(arr)):
+        if founder == arr[i]:
+            print(i)
+            break
+    print(i)
+    for j in range(len(binarr)):
+        if founder == binarr[j]:
+            print(j)
+            break
+    print(j)
+    g.write(str(i) + ' ' + str(j) + '\n')
+    g.close()
