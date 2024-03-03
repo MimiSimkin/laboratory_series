df62f2ee4a8ddbee754942ae87b25c9d57ff38d5
MimiSimkin <95422645+MimiSimkin@users.noreply.github.com>
2024-03-03 20:40:36 +0300

    Update obrab_lab1.py

--- obrab_lab1.py
+++ obrab_lab1.py
@@ -8,10 +8,13 @@ for i in range(50):
 for i in range(50):
     b.append(float(t[i].split()[1]))
 
-x = np.array(a)
-y = np.array(b)
+x1 = np.array(range(1000, 11000, 200))
+x2 = np.array(range(1000, 11000, 200))
+y1 = np.array(a)
+y2 = np.array(b)
 
-plt.errorbar(x, y,  xerr = 0, yerr = 0, fmt='o', color='b')
+plt.errorbar(x1, y1,  xerr = 0, yerr = 0, fmt='o', color='b')
+plt.errorbar(x2, y2,  xerr = 0, yerr = 0, fmt='o', color='r')
 plt.grid()
 plt.ylabel('$T, c$')
 plt.xlabel('$N, эл$')
